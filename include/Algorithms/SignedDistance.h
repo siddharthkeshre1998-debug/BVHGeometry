06d2b4c330ed3189ee4cb09c47c4df9e1167e1de
siddharthkeshre1998-debug <siddharthkeshre1998@gmail.com>
2026-07-29 23:54:34 +0530

    Update SignedDistance.h

--- include/Algorithms/SignedDistance.h
+++ include/Algorithms/SignedDistance.h
@@ -1,1 +1,20 @@
+#pragma once
 
+#include <limits>
+#include <memory>
+#include <vector>
+
+#include "BVH/BVH.h" 
+
+namespace bvh
+{
+bool SignedDistance::IsInside(const Vec3& point,
+                              const BVH& bvh)
+{
+  //Random direction for now
+  Vec direction(1.0, 0.3723, 0.6180).normalized();
+  Ray ray(point, direction);
+
+  return bvh.CountRayIntersections(ray) % 2);
+}
+} //namespace bvh
